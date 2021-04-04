fb1e10de48bf87132f00d847d338a2b000241474
Jonathan Bedon <bedonjonathan@gmail.com>
2021-04-04 17:20:41 -0400

    done no extra points yet

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./ContactList.css";
 
-const ContactList = () => {
-  return (
-    <div className="ContactList" data-testid="contact-list">
+
+class ContactList extends React.Component{
+  constructor(){
+  super()
+  }
+  render(){
+    return(
+      <div className="ContactList" data-testid="contact-list">
       <p>Contacts</p>
       <ul>
         <li>Andrew Clark</li>
@@ -11,8 +16,26 @@ const ContactList = () => {
         <li>Dan Abramov</li>
         <li>Flarnie Marchan</li>
       </ul>
-    </div>
-  );
-};
+    </div> 
+    )
+  }
+}
+
+
+
+
+// const ContactList = () => {
+//   return (
+//     <div className="ContactList" data-testid="contact-list">
+//       <p>Contacts</p>
+//       <ul>
+//         <li>Andrew Clark</li>
+//         <li>Brian Vaughn</li>
+//         <li>Dan Abramov</li>
+//         <li>Flarnie Marchan</li>
+//       </ul>
+//     </div>
+//   );
+// };
 
 export default ContactList;
