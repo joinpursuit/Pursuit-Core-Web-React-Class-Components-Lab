c9d6d74ccb39606f296e087e112cd6588d6e50ec
anjuhypp <anjuhypp@gmail.com>
2021-04-04 17:09:11 -0400

    clean code

--- src/Feed.js
+++ src/Feed.js
@@ -3,21 +3,6 @@ import Ad from "./Ad.js";
 import Post from "./Post.js";
 import "./Feed.css";
 
-// const Feed = () => {
-//   return (
-//     <div className="Feed" data-testid="feed">
-//       <h2>Feed</h2>
-//       <Post />
-//       <Post />
-//       <Ad />
-//       <Post />
-//       <Ad />
-//       <Post />
-//     </div>
-//   );
-// };
-
-
 class Feed extends React.Component {
   constructor() {
     super()
