ed99906889cd005ffdf6c83f8231d755f7c0b297
anjuhypp <anjuhypp@gmail.com>
2021-04-04 16:41:09 -0400

    refactor ad feed post

--- src/Post.js
+++ src/Post.js
@@ -7,14 +7,28 @@ const postInfo = {
   description: "This is the description of the post",
 };
 
-const Post = () => {
-  return (
-    <div className="Post" data-testid="post">
-      <p>{postInfo.title}</p>
-      <img src={postInfo.imageLink} alt="post" />
-      <p>{postInfo.description}</p>
-    </div>
-  );
-};
+// const Post = () => {
+//   return (
+//     <div className="Post" data-testid="post">
+//       <p>{postInfo.title}</p>
+//       <img src={postInfo.imageLink} alt="post" />
+//       <p>{postInfo.description}</p>
+//     </div>
+//   );
+// };
 
+class Post extends React.Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className="Post" data-testid="post">
+        <p>{postInfo.title}</p>
+        <img src={postInfo.imageLink} alt="post" />
+        <p>{postInfo.description}</p>
+      </div>
+    );
+  }
+}
 export default Post;
