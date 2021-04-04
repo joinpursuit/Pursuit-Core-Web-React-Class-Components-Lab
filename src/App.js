57c6f35dc5ec93bab49a4e0f38a82b501078c3c5
joshuamendez <joshuafmendez@gmail.com>
2021-04-03 23:10:53 -0400

    lab complete and bonus 1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./App.css";
 import ContactList from "./ContactList";
 import Feed from "./Feed";
+import Profile from "./Profile";
 
-function App() {
-  return (
-    <div className="App-Div">
-      <Feed />
-      <ContactList />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App-Div">
+        <Feed />
+        <ContactList />
+        <Profile />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/ContactList.js
+++ src/ContactList.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./ContactList.css";
 
-const ContactList = () => {
-  return (
-    <div className="ContactList" data-testid="contact-list">
-      <p>Contacts</p>
-      <ul>
-        <li>Andrew Clark</li>
-        <li>Brian Vaughn</li>
-        <li>Dan Abramov</li>
-        <li>Flarnie Marchan</li>
-      </ul>
-    </div>
-  );
-};
+class ContactList extends React.Component {
+  render() {
+    return (
+      <div className="ContactList" data-testid="contact-list">
+        <p>Contacts</p>
+        <ul>
+          <li>Andrew Clark</li>
+          <li>Brian Vaughn</li>
+          <li>Dan Abramov</li>
+          <li>Flarnie Marchan</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default ContactList;
