80b4e56ccab1c040fd3babf1454db36c8502b880
Yesenia L <ylopezcr123@gmail.com>
2021-04-06 17:30:18 -0400

    done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import "./App.css";
 import ContactList from "./ContactList";
 import Feed from "./Feed";
 
-function App() {
-  return (
-    <div className="App-Div">
-      <Feed />
-      <ContactList />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App-Div">
+//       <Feed />
+//       <ContactList />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+
+///////////////////////////////////
+
+// import React, { Component } from 'react'
+
+ class App extends React.Component {
+  constructor() {
+    super();
+
+  }
+  render() {
+    return (
+ <div className = "App-Div">
+ <Feed />
+ <ContactList />
+ </div>
+    ) 
+  }
+}
+ export default App;
--- src/ContactList.js
+++ src/ContactList.js
@@ -1,9 +1,33 @@
-import React from "react";
+// import React from "react";
 import "./ContactList.css";
 
-const ContactList = () => {
-  return (
-    <div className="ContactList" data-testid="contact-list">
+// const ContactList = () => {
+//   return (
+//     <div className="ContactList" data-testid="contact-list">
+//       <p>Contacts</p>
+//       <ul>
+//         <li>Andrew Clark</li>
+//         <li>Brian Vaughn</li>
+//         <li>Dan Abramov</li>
+//         <li>Flarnie Marchan</li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default ContactList;
+
+///////////////////
+
+import React, { Component } from 'react'
+
+export default class ContactList extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className="ContactList" data-testid="contact-list">
       <p>Contacts</p>
       <ul>
         <li>Andrew Clark</li>
@@ -12,7 +36,7 @@ const ContactList = () => {
         <li>Flarnie Marchan</li>
       </ul>
     </div>
-  );
-};
+    )
+  }
+}
 
-export default ContactList;
