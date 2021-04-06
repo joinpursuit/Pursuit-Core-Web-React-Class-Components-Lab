39b372af64a6a27cbe99453b0a78f5fb115b9259
Hady Mohamed <hadymohamed@pursuit.org>
2021-04-06 16:37:47 -0400

    Fun Lab Classes are Awesome

--- src/Feed.js
+++ src/Feed.js
@@ -1,20 +1,41 @@
-import React from "react";
+// import React from "react";
 import Ad from "./Ad.js";
 import Post from "./Post.js";
 import "./Feed.css";
 
-const Feed = () => {
-  return (
-    <div className="Feed" data-testid="feed">
-      <h2>Feed</h2>
-      <Post />
-      <Post />
-      <Ad />
-      <Post />
-      <Ad />
-      <Post />
-    </div>
-  );
-};
+// const Feed = () => {
+//   return (
+//     <div className="Feed" data-testid="feed">
+//       <h2>Feed</h2>
+//       <Post />
+//       <Post />
+//       <Ad />
+//       <Post />
+//       <Ad />
+//       <Post />
+//     </div>
+//   );
+// };
 
-export default Feed;
+// export default Feed;
+
+
+import React, { Component } from 'react'
+
+export default class Feed extends Component {
+  render() {
+    return (
+      <div>
+        <div className="Feed" data-testid="feed">
+         <h2>Feed</h2>
+           <Post />
+           <Post />
+           <Ad />
+           <Post />
+           <Ad />
+           <Post />
+         </div>
+      </div>
+    )
+  }
+}
