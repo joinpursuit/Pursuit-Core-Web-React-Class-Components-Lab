6077a6cf8d098420a73c3ddc5158c39a5d578b08
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-04-06 22:42:00 -0400

    done

--- src/Feed.js
+++ src/Feed.js
@@ -1,20 +1,26 @@
-import React from "react";
+
 import Ad from "./Ad.js";
 import Post from "./Post.js";
 import "./Feed.css";
 
-const Feed = () => {
-  return (
-    <div className="Feed" data-testid="feed">
-      <h2>Feed</h2>
-      <Post />
-      <Post />
-      <Ad />
-      <Post />
-      <Ad />
-      <Post />
-    </div>
-  );
-};
-
-export default Feed;
+import React, { Component } from 'react'
+export default class Feed extends Component {
+	constructor() {
+		super()
+	}
+	render() {
+		return (
+			<div>
+				<div className='Feed' data-testid='feed'>
+					<h2>Feed</h2>
+					<Post />
+					<Post />
+					<Ad />
+					<Post />
+					<Ad />
+					<Post />
+				</div>
+			</div>
+		)
+	}
+}
