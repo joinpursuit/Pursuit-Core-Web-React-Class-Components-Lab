59c4928e6c5021c4c3c0da82ac9a1f559744afa6
Jamee Hengari <jhengari@hotmail.com>
2021-04-08 15:30:28 -0400

    first react class lab commit

--- src/Ad.js
+++ src/Ad.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Ad.css";
 
 export const adInfo = [
@@ -16,16 +16,16 @@ export const adInfo = [
   },
 ];
 
-const Ad = () => {
-  const index = Math.floor(Math.random() * adInfo.length);
-  const ad = adInfo[index];
-
-  return (
-    <div className="Ad" data-testid="ad">
-      <img src={ad.src} alt="cool ad" />
-      <p data-testid="ad-text">{ad.text}</p>
-    </div>
-  );
-};
-
+export class Ad extends Component {
+  render() {
+    const index = Math.floor(Math.random() * adInfo.length);
+    const ad = adInfo[index];
+    return (
+      <div className="Ad" data-testid="ad">
+        <img src={ad.src} alt="cool ad" />
+        <p data-testid="ad-text">{ad.text}</p>
+      </div>
+    );
+  }
+}
 export default Ad;
--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import ContactList from "./ContactList";
 import Feed from "./Feed";
 
-function App() {
-  return (
-    <div className="App-Div">
-      <Feed />
-      <ContactList />
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="App-Div">
+        <Feed />
+        <ContactList />
+      </div>
+    );
+  }
 }
 
 export default App;
