bb186df4f56afd5fbaad0817f6f16db24508133e
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-04-07 20:38:15 -0400

    Tests passing, working on Bonus

--- src/Ad.js
+++ src/Ad.js
@@ -1,31 +1,37 @@
-import React from "react";
-import "./Ad.css";
+import React from 'react'
+import './Ad.css'
 
 export const adInfo = [
   {
-    src: "https://placeimg.com/200/200/arch",
-    text: "Doctors hate THIS ONE WEIRD TRICK",
+    src: 'https://placeimg.com/200/200/arch',
+    text: 'Doctors hate THIS ONE WEIRD TRICK'
   },
   {
-    src: "https://placeimg.com/200/200/nature",
-    text: "You won't believe what happened next!",
+    src: 'https://placeimg.com/200/200/nature',
+    text: "You won't believe what happened next!"
   },
   {
-    src: "http://placekitten.com/g/200/200",
-    text: "Celebs: Where are they now??",
-  },
-];
+    src: 'http://placekitten.com/g/200/200',
+    text: 'Celebs: Where are they now??'
+  }
+]
 
-const Ad = () => {
-  const index = Math.floor(Math.random() * adInfo.length);
-  const ad = adInfo[index];
+class Ad extends React.Component {
+  constructor () {
+    super()
 
-  return (
-    <div className="Ad" data-testid="ad">
-      <img src={ad.src} alt="cool ad" />
-      <p data-testid="ad-text">{ad.text}</p>
-    </div>
-  );
-};
+    this.index = Math.floor(Math.random() * adInfo.length)
+    this.ad = adInfo[this.index]
+  }
 
-export default Ad;
+  render () {
+    return (
+      <div className='Ad' data-testid='ad'>
+        <img src={this.ad.src} alt='cool ad' />
+        <p data-testid='ad-text'>{this.ad.text}</p>
+      </div>
+    )
+  }
+}
+
+export default Ad
