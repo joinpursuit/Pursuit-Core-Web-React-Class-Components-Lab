d6b54b86f449bcc133814187810755aaa9f3e7be
Obaid Rustemi <orustemi@gmail.com>
2021-04-09 14:04:20 -0400

    dead code cleanup

--- src/Ad.js
+++ src/Ad.js
@@ -16,19 +16,7 @@ export const adInfo = [
   },
 ];
 
-// const Ad = () => {
-//   const index = Math.floor(Math.random() * adInfo.length);
-//   const ad = adInfo[index];
 
-//   return (
-//     <div className="Ad" data-testid="ad">
-//       <img src={ad.src} alt="cool ad" />
-//       <p data-testid="ad-text">{ad.text}</p>
-//     </div>
-//   );
-// };
-
-// export default Ad;
 
 class Ad extends React.Component{
 //   constructor (adInfo) {
