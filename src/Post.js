d1678fc114470868fe58b563d1b98841e0ab7634
Ivan Castillo <total180training@gmail.com>
2021-04-11 05:44:14 -0400

    Completed all tests. Might return for bonuses

--- src/Post.js
+++ src/Post.js
@@ -1,20 +1,43 @@
 import React from "react";
 import "./Post.css";
 
-const postInfo = {
-  title: "Sample Post Title",
-  imageLink: "https://www.stockvault.net/data/2007/03/01/100169/preview16.jpg",
-  description: "This is the description of the post",
-};
+// const postInfo = {
+//   title: "Sample Post Title",
+//   imageLink: "https://www.stockvault.net/data/2007/03/01/100169/preview16.jpg",
+//   description: "This is the description of the post",
+// };
+// const Post = () => {
+//   return (
+//     <div className="Post" data-testid="post">
+//       <p>{postInfo.title}</p>
+//       <img src={postInfo.imageLink} alt="post" />
+//       <p>{postInfo.description}</p>
+//     </div>
+//   );
+// };
 
-const Post = () => {
-  return (
-    <div className="Post" data-testid="post">
-      <p>{postInfo.title}</p>
-      <img src={postInfo.imageLink} alt="post" />
-      <p>{postInfo.description}</p>
-    </div>
-  );
-};
+
+class Post extends React.Component {
+  constructor() {
+    super()
+
+    this.title = "Sample Post Title"
+    this.imageLink = "https://www.stockvault.net/data/2007/03/01/100169/preview16.jpg"
+    this.description = "This is the description of the post"
+  }
+
+  render() {
+    return (
+      <div className="Post" data-testid="post">
+        <p>{this.title}</p>
+        <img src={this.imageLink} alt="post"/>
+        <p>{this.description}</p>
+      </div>
+    )
+  }
+
+}
+
+
 
 export default Post;
