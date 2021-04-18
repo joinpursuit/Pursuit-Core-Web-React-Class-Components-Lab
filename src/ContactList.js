55723024684ea63973648174d9942360b847bda7
Sarah Weinstein <sbweins@gmail.com>
2021-04-17 23:32:33 -0400

    Lab Update

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,18 +1,38 @@
 import React from "react";
 import "./ContactList.css";
+import Contact from "./Contact.js";
 
-const ContactList = () => {
-  return (
-    <div className="ContactList" data-testid="contact-list">
-      <p>Contacts</p>
-      <ul>
-        <li>Andrew Clark</li>
-        <li>Brian Vaughn</li>
-        <li>Dan Abramov</li>
-        <li>Flarnie Marchan</li>
-      </ul>
-    </div>
-  );
-};
+class ContactList extends React.Component {
+  render() {
+    return (
+      <div className="ContactList" data-testid="contact-list">
+        <p>Contacts</p>
+        <ul>
+          <li>Andrew Clark</li>
+          <li>Brian Vaughn</li>
+          <li>Dan Abramov</li>
+          <li>Flarnie Marchan</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default ContactList;
+
+// edit with props test on contact.js
+// class ContactList extends React.Component {
+//   render () {
+//     return (
+//       <div className="ContactList" data-testid="contact-list">
+//         <p>Contacts</p>
+//         <ul>
+//           <Contact name="Andrew Clarke"/>
+//           <Contact name="Brian Vaughn"/>
+//           <li>Dan Abramov</li>
+//           <li>Flarnie Marchan</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
--- src/Feed.js
+++ src/Feed.js
@@ -3,18 +3,20 @@ import Ad from "./Ad.js";
 import Post from "./Post.js";
 import "./Feed.css";
 
-const Feed = () => {
-  return (
-    <div className="Feed" data-testid="feed">
-      <h2>Feed</h2>
-      <Post />
-      <Post />
-      <Ad />
-      <Post />
-      <Ad />
-      <Post />
-    </div>
-  );
-};
+class Feed extends React.Component {
+  render() {
+    return (
+      <div className="Feed" data-testid="feed">
+        <h2>Feed</h2>
+        <Post />
+        <Post />
+        <Ad />
+        <Post />
+        <Ad />
+        <Post />
+      </div>
+    );
+  }
+}
 
 export default Feed;
